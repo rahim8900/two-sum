f5602f4f4681cf2f1884e26abcb99c270c511568
rahim8900 <rehimhesenov26@gmail.com>
2024-05-08 15:27:05 +0400

    Add project files.

---
+++ Two-sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Two_sum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int target = Convert.ToInt32(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(',');
+            int i, j;
+            int r1 = 0;
+            int r2 = 0;
+            for (i = 0; i < input.Length; i++)
+            {
+                for (j = 0; j < input.Length; j++)
+                {
+                    if ((i != j) && ((int.Parse(input[i]) + int.Parse(input[j])) == target))
+                    {
+                        r1 = i;
+                        r2 = j;
+                    }
+                }
+            }
+            Console.WriteLine($"[{r2}, {r1}]");
+            Console.ReadKey();
+        }
+    }
+}
